9d8c713b0363f13c2af1e924d087eb09ce4496d8
Anthony <aquinoanthony851@gmail.com>
2024-05-03 14:23:19 -0600

    RetirIEving data from an API

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,25 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://cat-fact.herokuapp.com/facts" # Used a different api due to inability to access http://data.cityofnewyork.us/resource/uvks-tn5n.json
 
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["text"])
+
+        return programs_list
+
+#programs = GetPrograms().get_programs()
+#print(programs)
+
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
